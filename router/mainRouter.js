bef115a3de33a7fef54997b70876bb71c0ec77e4
Remigijus66 <remigijus.bartaska@gmail.com>
2023-09-22 16:09:22 +0300

    init

---
+++ router/mainRouter.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const router = express.Router()
+const { validateRegistration, validateAuction } = require("../middleware/validator")
+
+const {
+    register,
+    login,
+    logout,
+    checksesssion,
+    validate,
+    upload,
+    downloadAll,
+    downloadSingle,
+    placeBid,
+} = require("../controllers/mainController")
+
+router.post('/register', validateRegistration, register)
+router.post('/login', login)
+router.post('/logout', logout)
+router.post('/checksesssion', checksesssion)
+router.post('/validate', validateAuction, validate)
+router.post('/upload', upload)
+router.post('/downloadAll', downloadAll)
+router.post('/downloadSingle', downloadSingle)
+router.post('/placeBid', placeBid)
+
+
+module.exports = router
